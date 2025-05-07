96461d80c4ca492318d785fd5c994c09a8f760ad
Angelo Garcia <angelo.garcia@stu.ocsb.ca>
2025-05-07 15:03:36 +0000

    Sentence about what I did

--- js/script.js
+++ js/script.js
@@ -1,28 +1,12 @@
-// Global variable to hold the random number
-let randomNumber;
-
-// This function will generate the random number once when the page loads
-window.onload = function () {
-  // Generate a random number between 1 and 10
-  randomNumber = Math.floor(Math.random() * 10) + 1;
-  console.log("Random number generated: " + randomNumber); // For testing purposes
-}
-
-// This function is triggered when the user clicks "Check the results"
-function checkGuess() {
-  // Get the user's guess from the input field
-  const userGuess = parseInt(document.getElementById("guess-input").value);
-
-  // Validate if the guess is a valid number and within the range 1-10
-  if (isNaN(userGuess) || userGuess < 1 || userGuess > 10) {
-    document.getElementById("answers").innerHTML = "Please enter a valid number between 1 and 10.";
-    return; // Stop the function if the input is invalid
-  }
-
-  // Check if the user's guess is correct
-  if (userGuess === randomNumber) {
-    document.getElementById("answers").innerHTML = `Congratulations! You guessed the correct number: ${randomNumber}`;
-  } else {
-    document.getElementById("answers").innerHTML = `Oops! The correct number was ${randomNumber}. Try again!`;
-  }
+/* Created by: Angelo Garcia
+* Created on: May 6, 2025
+* This file contains the CSS for index.html
+*/
+// Function to generate a random number between 1 and 6
+function generateRandomNumber() {
+  // Generate a random number between 1 and 6
+  var correctGuess = Math.floor(Math.random() * 6) + 1;
+  
+  // Display the result
+  document.getElementById("result").innerHTML = "The random number is: " + correctGuess;
 }
